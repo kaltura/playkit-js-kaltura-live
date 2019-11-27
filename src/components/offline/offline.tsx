3d4c983b78417a1c1966d58cf58cb1d56220a838
Eitan Avgil <eitan.avgil@kaltura.com>
2019-11-27 16:04:12 +0200

    fix: add support for http-error string in offline slate

--- src/components/offline/offline.tsx
+++ src/components/offline/offline.tsx
@@ -1,13 +1,35 @@
 import { h, Component } from "preact";
 import * as styles from "./offline.scss";
 
-export class Offline extends Component {
-    render() {
+export interface props {
+    httpError?: boolean;
+}
+
+export class Offline extends Component<props> {
+    static defaultProps = {
+        httpError: false
+    };
+
+    render(props: props) {
         return (
             <div className={styles.offlineWrapper}>
                 <div className={`${styles.offlineIcon} kaltura-live__offline-icon`} />
-                <p className={styles.primaryText}>Currently not broadcasting</p>
-                <p className={styles.secondaryText}>video will play once broadcasting starts</p>
+                {!props.httpError && (
+                    <div>
+                        <p className={styles.primaryText}>Currently not broadcasting</p>
+                        <p className={styles.secondaryText}>
+                            video will play once broadcasting starts
+                        </p>
+                    </div>
+                )}
+                {props.httpError && (
+                    <div>
+                        <p className={styles.primaryText}>No Internet Connection</p>
+                        <p className={styles.secondaryText}>
+                            Check your network and refresh the page
+                        </p>
+                    </div>
+                )}
             </div>
         );
     }
