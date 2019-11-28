5fb3b731960c7f275a2c6b9fb2d8f9139eb256b3
Eitan Avgil <eitan.avgil@kaltura.com>
2019-11-28 09:03:37 +0200

    Chore: shorter code

--- src/components/offline/offline.tsx
+++ src/components/offline/offline.tsx
@@ -9,27 +9,20 @@ export class Offline extends Component<props> {
     static defaultProps = {
         httpError: false
     };
-
     render(props: props) {
         return (
             <div className={styles.offlineWrapper}>
                 <div className={`${styles.offlineIcon} kaltura-live__offline-icon`} />
-                {!props.httpError && (
-                    <div>
-                        <p className={styles.primaryText}>Currently not broadcasting</p>
-                        <p className={styles.secondaryText}>
-                            video will play once broadcasting starts
-                        </p>
-                    </div>
-                )}
-                {props.httpError && (
-                    <div>
-                        <p className={styles.primaryText}>No Internet Connection</p>
-                        <p className={styles.secondaryText}>
-                            Check your network and refresh the page
-                        </p>
-                    </div>
-                )}
+                <div>
+                    <p className={styles.primaryText}>
+                        {props.httpError ? "No Internet Connection" : "Currently not broadcasting"}
+                    </p>
+                    <p className={styles.secondaryText}>
+                        {props.httpError
+                            ? "Check your network and refresh the page"
+                            : "Video will play once broadcasting starts"}
+                    </p>
+                </div>
             </div>
         );
     }
