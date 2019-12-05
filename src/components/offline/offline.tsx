3ca1e2728e30a1a33bed268f7ee333504d15ed2f
Eitan <eitan.avgil@kaltura.com>
2019-12-05 10:25:52 +0200

    fix: FEV-427 - improve errors message (#33)
    * fix: FEV-427 - improve errors message
    
    * fix: FEV-427 - improve errors message

--- src/components/offline/offline.tsx
+++ src/components/offline/offline.tsx
@@ -5,22 +5,36 @@ export interface props {
     httpError?: boolean;
 }
 
+const noInternetTitle = "No Internet Connection";
+const noInternetBody = "Check your network and refresh the page";
+const offlineTitle = "Currently not broadcasting";
+const offlineBody = "Video will play once broadcasting starts";
+const httpProblemTitle = "Something went wrong";
+const httpProblemBody = "Try refreshing the page";
+
 export class Offline extends Component<props> {
     static defaultProps = {
         httpError: false
     };
+
     render(props: props) {
         return (
             <div className={styles.offlineWrapper}>
                 <div className={`${styles.offlineIcon} kaltura-live__offline-icon`} />
                 <div>
                     <p className={styles.primaryText}>
-                        {props.httpError ? "No Internet Connection" : "Currently not broadcasting"}
+                        {props.httpError
+                            ? navigator.onLine
+                                ? httpProblemTitle
+                                : noInternetTitle
+                            : offlineTitle}
                     </p>
                     <p className={styles.secondaryText}>
                         {props.httpError
-                            ? "Check your network and refresh the page"
-                            : "Video will play once broadcasting starts"}
+                            ? navigator.onLine
+                                ? httpProblemBody
+                                : noInternetBody
+                            : offlineBody}
                     </p>
                 </div>
             </div>
