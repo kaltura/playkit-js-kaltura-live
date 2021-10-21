1de4154033cbc341c995d8a5ac56c1f51f2f8241
Serhii <serhii.marchenko@Kaltura.com>
2021-10-21 14:43:51 +0300

    fix(FEV-1081): handle 'end' event on switches from primary\secondary (#334)
    * fix(FEV-1081): handle 'end' event on switches from primary\secondary
    
    * fix(FEV-1081): address PR comments

--- src/decorator/live-decorator.ts
+++ src/decorator/live-decorator.ts
@@ -1,24 +1,33 @@
 import { KalturaLivePlugin } from '../kaltura-live-plugin';
 import { getContribLogger } from '@playkit-js-contrib/common';
 
-const HTTP_ERRORS = [1002, 1003, 3016, 3022];
+const { Error } = KalturaPlayer.core as any;
+const HANDLED_ERRORS = [
+  Error.Code.HTTP_ERROR,
+  Error.Code.TIMEOUT,
+  Error.Code.LIVE_MANIFEST_REFRESH_ERROR,
+  Error.Code.VIDEO_ERROR,
+  Error.Code.NATIVE_ADAPTER_LOAD_FAILED,
+];
 
 const logger = getContribLogger({
   class: 'KalturaLiveDecorator',
   module: 'kaltura-live-decorator',
 });
 
-export class KalturaLiveEngineDecorator 
+export class KalturaLiveEngineDecorator
   implements KalturaPlayerTypes.IEngineDecorator {
   _plugin: KalturaLivePlugin;
   _engine: any;
   _dispatcher: Function;
   _hadError: boolean;
+  _ended: boolean;
 
   constructor(engine: any, plugin: KalturaLivePlugin, dispatcher: Function) {
     this._plugin = plugin;
     this._engine = engine;
     this._hadError = false;
+    this._ended = false;
     this._dispatcher = dispatcher;
 
     this._plugin.player.addEventListener(
@@ -30,6 +39,11 @@ export class KalturaLiveEngineDecorator
       this._plugin.player.Event.ERROR,
       this._handleError
     );
+
+    this._engine.addEventListener(
+      this._plugin.player.Event.ENDED,
+      this._handleEnd
+    );
   }
 
   private _handleError = (e: any) => {
@@ -39,20 +53,31 @@ export class KalturaLiveEngineDecorator
     this._hadError = true;
   };
 
+  private _handleEnd = () => {
+    this._plugin.detachMediaSource();
+    this._plugin.updateLiveStatus();
+    this._ended = true;
+  };
+
   private _handleMediaLoaded = () => {
     this._hadError = false;
+    this._ended = false;
   };
 
   get active(): boolean {
-    return this._plugin.isMediaLive && this._hadError;
+    return this._plugin.isMediaLive && (this._hadError || this._ended);
   }
 
   dispatchEvent(event: any): any {
+    if (event.type === this._plugin.player.Event.ENDED) {
+      this._ended = false;
+      return true;
+    }
     if (
       event.type === 'error' &&
       event.payload &&
       event.payload.code &&
-      HTTP_ERRORS.indexOf(event.payload.code) > -1
+      HANDLED_ERRORS.indexOf(event.payload.code) > -1
     ) {
       logger.info(
         `Kaltura live-decorator prevented interrupted error ${event.payload.code}`,
