99a4047d3abdf2069d5747156b6b9f7fd7850712
Serhii <serhii.marchenko@Kaltura.com>
2022-07-01 14:44:28 +0300

    fix(Fev 1278 ): If the playlist contains a live entry that is not broadcasting the currently not broadcasting tile will overlap the entire player (#357)
    * fix(FEV-1278): upd dependencies
    
    * fix(FEV-1278): live layout fix
    
    * fix(FEV-1278): dev comments and attach media on reset

--- src/components/offline-slate/offline-slate.tsx
+++ src/components/offline-slate/offline-slate.tsx
@@ -1,6 +1,7 @@
 import {h, Component} from 'preact';
 import {Offline} from '../offline';
 import {NoLongerLive} from '../no-longer-live';
+import './offline-slate.scss';
 
 export enum OfflineTypes {
   None = 'None',
@@ -13,12 +14,47 @@ interface OfflineSlateState {
   type: OfflineTypes;
 }
 
-interface OfflineSlateProps {}
+interface OfflineSlateProps {
+  addPlayerClass?: () => void;
+  removePlayerClass?: () => void;
+  removeSpinner?: () => void;
+}
 
+const LIVE_PLUGIN_HAS_OVERLAY_CLASSNAME = 'has-live-plugin-overlay';
+
+const {
+  redux: {connect},
+  reducers: {shell, loading}
+} = KalturaPlayer.ui;
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    addPlayerClass: () => dispatch(shell.actions.addPlayerClass(LIVE_PLUGIN_HAS_OVERLAY_CLASSNAME)),
+    removePlayerClass: () => dispatch(shell.actions.removePlayerClass(LIVE_PLUGIN_HAS_OVERLAY_CLASSNAME)),
+    removeSpinner: () => dispatch(loading.actions.updateLoadingSpinnerState(false))
+  };
+};
+
+@connect(null, mapDispatchToProps, null, {forwardRef: true})
 export class OfflineSlate extends Component<OfflineSlateProps, OfflineSlateState> {
   state = {
     type: OfflineTypes.None
   };
+
+  componentWillUnmount() {
+    this.props.removePlayerClass!();
+  }
+
+  componentDidUpdate(prevProps: OfflineSlateProps, prevState: OfflineSlateState) {
+    if (prevState.type !== this.state.type) {
+      if (this.state.type !== OfflineTypes.None) {
+        this.props.addPlayerClass!();
+        this.props.removeSpinner!();
+      } else {
+        this.props.removePlayerClass!();
+      }
+    }
+  }
 
   public manageOfflineSlate = (type: OfflineTypes) => {
     if (type !== this.state.type) {
