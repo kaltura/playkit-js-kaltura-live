c0583b5c0a5e5f9a387f973181a11cb861b2f092
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2022-10-27 12:56:13 +0300

    fix(FEV-1546): Translation in live plugin

---
+++ src/components/live-tag/live-tag-view.tsx
@@ -0,0 +1,40 @@
+import {h, Component} from 'preact';
+import * as styles from './live-tag.scss';
+import {LiveTagStates} from './live-tag';
+import {A11yWrapper} from '@playkit-js/common';
+const {withText, Text} = KalturaPlayer.ui.preacti18n;
+
+
+export interface props {
+    seekToLiveEdge: Function;
+    getStyles:Function;
+    behindLiveEdge: boolean;
+    liveTagState: LiveTagStates;
+    offline?: string;
+    live?: string;
+    preview?: string;
+}
+
+const translates = {
+    live: <Text id="kalturaLive.live">Live</Text>,
+    offline: <Text id="kalturaLive.offline">Offline</Text>,
+    preview: <Text id="kalturaLive.preview">Preview</Text>
+};
+
+@withText(translates)
+export class LiveTagView extends Component<props> {
+
+    render(props: props) {
+        return (
+          <A11yWrapper onClick={() =>props.seekToLiveEdge()}>
+            <div
+              role="button"
+              tab-index={0}
+              className={[styles.liveTag, props.getStyles(), props.behindLiveEdge ? styles.clickable : ''].join(' ')}
+              aria-label={props.liveTagState}>
+              {props[props.liveTagState]}
+            </div>
+         </A11yWrapper>
+        );
+    }
+}
