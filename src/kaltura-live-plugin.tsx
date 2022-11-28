f8404f195d5fd9bf28d9f8ce881778c58ad35d88
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-11-28 14:04:36 +0200

    chore: change KPlayer network configuration (use default value)

--- src/kaltura-live-plugin.tsx
+++ src/kaltura-live-plugin.tsx
@@ -70,9 +70,6 @@ export class KalturaLivePlugin extends KalturaPlayer.core.BasePlugin implements 
 
   loadMedia(): void {
     this.player.configure({
-      network: {
-        maxStaleLevelReloads: 19 // more details in https://kaltura.atlassian.net/browse/FEV-1205
-      },
       playback: {
         options: {
           html5: {
