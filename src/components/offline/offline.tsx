772acce9eb1ad45484174f15b19b5671757f2f89
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2022-12-05 17:59:52 +0200

    fix(FEV-1593): live screens content is not being read by screen readers (#375)
    **the issue:**
    content of live screens (i.e. offline, no longer live, etc) is not being read by screen readers.
    
    **solution:**
    add `aria-live=polite` 
    
    Solves [FEV-1593](https://kaltura.atlassian.net/browse/FEV-1593)

--- src/components/offline/offline.tsx
+++ src/components/offline/offline.tsx
@@ -4,7 +4,6 @@ import {OfflineIcon} from './offline-icon';
 import {NetworkIssueIcon} from './network-issue-icon';
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
 
-
 export interface props {
   httpError?: boolean;
   noInternetTitle?: string;
@@ -16,13 +15,12 @@ export interface props {
 }
 
 const translates = {
-    noInternetTitle: <Text id="kalturaLive.no_internet_title">No Internet Connection</Text>,
-    noInternetBody: <Text id="kalturaLive.no_internet_body">Check your network</Text>,
-    offlineTitle: <Text id="kalturaLive.offline_title">Currently not broadcasting</Text>,
-    offlineBody: <Text id="kalturaLive.offline_body">Video will play once broadcasting starts</Text>,
-    httpProblemTitle: <Text id="kalturaLive.http_problem_title">Something went wrong</Text>,
-    httpProblemBody: <Text id="kalturaLive.http_problem_body">Try refreshing the page</Text>
-
+  noInternetTitle: <Text id="kalturaLive.no_internet_title">No Internet Connection</Text>,
+  noInternetBody: <Text id="kalturaLive.no_internet_body">Check your network</Text>,
+  offlineTitle: <Text id="kalturaLive.offline_title">Currently not broadcasting</Text>,
+  offlineBody: <Text id="kalturaLive.offline_body">Video will play once broadcasting starts</Text>,
+  httpProblemTitle: <Text id="kalturaLive.http_problem_title">Something went wrong</Text>,
+  httpProblemBody: <Text id="kalturaLive.http_problem_body">Try refreshing the page</Text>
 };
 @withText(translates)
 export class Offline extends Component<props> {
@@ -32,11 +30,17 @@ export class Offline extends Component<props> {
 
   render(props: props) {
     return (
-      <div className={styles.offlineWrapper}>
-        <div className={styles.offlineIcon}>{navigator.onLine ? <OfflineIcon /> : <NetworkIssueIcon />}</div>
-        <div>
-          <p className={styles.primaryText}>{props.httpError ? (navigator.onLine ? props.httpProblemTitle : props.noInternetTitle) : props.offlineTitle}</p>
-          <p className={styles.secondaryText}>{props.httpError ? (navigator.onLine ? props.httpProblemBody : props.noInternetBody) : props.offlineBody}</p>
+      <div className={styles.offlineWrapper} role="banner">
+        <div className={styles.offlineIcon} aria-hidden="true">
+          {navigator.onLine ? <OfflineIcon /> : <NetworkIssueIcon />}
+        </div>
+        <div role="contentinfo">
+          <p className={styles.primaryText}>
+            {props.httpError ? (navigator.onLine ? props.httpProblemTitle : props.noInternetTitle) : props.offlineTitle}
+          </p>
+          <p className={styles.secondaryText}>
+            {props.httpError ? (navigator.onLine ? props.httpProblemBody : props.noInternetBody) : props.offlineBody}
+          </p>
         </div>
       </div>
     );
--- src/kaltura-live-plugin.tsx
+++ src/kaltura-live-plugin.tsx
@@ -122,7 +122,7 @@ export class KalturaLivePlugin extends KalturaPlayer.core.BasePlugin implements 
       presets: ['Live', 'Playback'],
       container: 'VideoArea',
       get: () => {
-        return <OfflineSlate ref={this._offlineSlate} />;
+        return <OfflineSlate ref={this._offlineSlate} getGuiAreaNode={this._getGuiAreaNode} />;
       }
     });
   };
@@ -275,6 +275,11 @@ export class KalturaLivePlugin extends KalturaPlayer.core.BasePlugin implements 
       }
     });
   }
+
+  private _getGuiAreaNode = () => {
+    const targetId = document.getElementById(this.player.config.targetId!) || document;
+    return targetId.querySelector('.playkit-gui-area') || null;
+  };
 
   // The function calls 'isLive' api and then repeats the call every X seconds (10 by default)
   public updateLiveStatus = () => {
