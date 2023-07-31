9b8a005c9b487c74b844149c8f0861697e71d5d6
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2023-07-31 19:10:38 +0300

    feat(FEC-13247): add e2e tests

--- cypress/e2e/kaltura-live.cy.ts
+++ cypress/e2e/kaltura-live.cy.ts
@@ -25,13 +25,13 @@ describe('Kaltura-live plugin', () => {
         cy.get('[data-testid="kaltura-live_offlineSlate"]').should('not.exist');
       });
     });
-    it('should render currently not broadcasting slate', () => {
+    it('should render pre-playback slate', () => {
       mockKalturaBe('live.json', 'offline-stream.json');
       loadPlayer().then(() => {
         cy.get('[data-testid="kaltura-live_offlineWrapper"]').should('exist');
-        cy.get('[data-testid="kaltura-live_offlineSlate"]')
-          .should('exist')
-          .should('have.text', 'Currently not broadcastingVideo will play once broadcasting starts');
+        cy.get('[data-testid="kaltura-live_offlineSlate"]').should('exist');
+        cy.get('.kaltura-live-title').should('have.text', 'Not broadcasting yet');
+        cy.get('.kaltura-live-description').should('have.text', 'Video will play once broadcasting starts');
       });
     });
     it('should render live tag', () => {
@@ -40,8 +40,40 @@ describe('Kaltura-live plugin', () => {
         cy.get('[data-testid="kaltura-live_liveTag"]').should('exist').should('have.text', 'Live');
       });
     });
+    it('should render post-playback slate', () => {
+      mockKalturaBe('live.json', 'live-stream.json');
+      loadPlayer().then(kalturaPlayer => {
+        kalturaPlayer.currentTime = 60;
+        cy.get('.kaltura-live-title').should('have.text', 'Broadcast is no longer live');
+        kalturaPlayer.dispatchEvent(new FakeEvent(EventType.ENDED));
+      });
+    });
+    it('should render entry poster as offline slate background', () => {
+      mockKalturaBe('live.json', 'offline-stream.json');
+      loadPlayer().then(kalturaPlayer => {
+        cy.get('[data-testid="kaltura-live_offlineWrapper"]').should('have.attr', 'style', `background-image: url("${kalturaPlayer.poster}");`);
+      });
+    });
+    it('should render custom image url as offline slate background', () => {
+      mockKalturaBe('live.json', 'offline-stream.json');
+      loadPlayer({offlineSlateUrl: 'http://test'}).then(() => {
+        cy.get('[data-testid="kaltura-live_offlineWrapper"]').should('have.attr', 'style', `background-image: url("http://test");`);
+      });
+    });
+    it('should render offline slate without text message', () => {
+      mockKalturaBe('live.json', 'offline-stream.json');
+      loadPlayer({offlineSlateWithoutText: true}).then(() => {
+        cy.get('[data-testid="kaltura-live_offlineWrapper"]')
+          .should('exist')
+          .then(() => {
+            cy.get('.kaltura-live-title').should('not.exist');
+            cy.get('.kaltura-live-description').should('not.exist');
+          });
+      });
+    });
     if (Cypress.browser.name !== 'webkit') {
-      it('should render error slate', () => {
+      // TODO: update test once kaltura-player got updated
+      it.skip('should render error slate', () => {
         mockKalturaBe('live.json', null);
         loadPlayer().then(() => {
           cy.get('[data-testid="kaltura-live_offlineWrapper"]').should('exist');
